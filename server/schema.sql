43054c2a871338188579781c446a58fe22cc113e
speckospock <ben.hq.stephens@gmail.com>
2017-09-18 15:52:44 -0700

    Implements .gitignore and server/schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -4,14 +4,28 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL,
+  createdAt datetime NOT NULL,
+  text varchar(240),
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (user) REFERENCES users(id),
+  FOREIGN KEY (room) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id int NOT NULL,
+  name varchar (20) NOT NULL,
+);
 
+CREATE TABLE users (
+  id int NOT NULL,
+  name varchar (20) NOT NULL,
+);
 
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
