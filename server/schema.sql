843ac6f3cfb640db4e18de34c3c246847b68317c
speckospock <ben.hq.stephens@gmail.com>
2017-09-18 16:50:01 -0700

    Implements database schema creation

--- server/schema.sql
+++ server/schema.sql
@@ -5,28 +5,24 @@ USE chat;
 CREATE TABLE messages (
   /* Describe your table here.*/
   id int NOT NULL,
-  user int,
-  room int,
+  user int NOT NULL,
+  room int NOT NULL,
   createdAt datetime NOT NULL,
-  text varchar(240),
+  message varchar(240) NOT NULL,
 
-  PRIMARY KEY (id),
-  FOREIGN KEY (user) REFERENCES users(id),
-  FOREIGN KEY (room) REFERENCES rooms(id)
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 CREATE TABLE rooms (
   id int NOT NULL,
-  name varchar (20) NOT NULL,
-
+  name varchar(20) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE users (
   id int NOT NULL,
-  name varchar (20) NOT NULL,
-
+  name varchar(20) NOT NULL,
   PRIMARY KEY (id)
 );
 
