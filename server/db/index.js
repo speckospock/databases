4d0d882d0dd27facc8b334ee9ac25813c03ecdf9
speckospock <ben.hq.stephens@gmail.com>
2017-09-19 12:40:40 -0700

    Implements mongodb

--- server/db/index.js
+++ server/db/index.js
@@ -1,26 +1,47 @@
-var mysql = require('mysql');
-var Sequelize = require('sequelize');
+const mongoClient = require('mongodb').MongoClient;
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
+const url = 'mongodb://localhost:27017/test';
+
+let id = 0;
+
+exports.readMessages = (callback, user = null) => {
+  mongoClient.connect(url, (err, db) => {
+    db.createCollection('messages', (err, collection) => {
+      if (!user) {
+        collection.find().toArray((err, documents) => {
+          console.log("READ: ", documents);
+          callback(documents);
+          db.close();
+        });
+      } else {
+        collection.find({username: user}).toArray((err, documents) => {
+          console.log("READ: ", documents);
+          callback(documents);
+          db.close();
+
+        });
+      }
+    });
+  });
+};
+
+exports.writeMessage = (message = '', username = 'anonymous', roomname = 'lobby') => {
+  mongoClient.connect(url, (err, db) => {
+    db.createCollection('messages', (err, collection) => {
+      collection.insert({username: username, text: message, roomname: roomname, objectId: id++});
+      console.log('inserted!!! HEEHEE');
+      db.close();
+    });
+  });
+};
 
 /*
 
-##################
-#from mysql docs:#
-##################
+///////////////////////////
+//// SEQUELIZE + MYSQL ////
+///////////////////////////
 
-The simplest form of .query() is .query(sqlString, callback), where a SQL string is the first argument and the second is a callback:
-
-connection.query('SELECT * FROM `books` WHERE `author` = "David"', function (error, results, fields) {
-  // error will be an Error if one occurred during the query
-  // results will contain the results of the query
-  // fields will contain information about the returned results fields (if any)
-});
-
-*/
-
+var mysql = require('mysql');
 var db = new Sequelize('chat', 'root', 'plantlife');
 
 var Message = db.define('messages', {
@@ -62,40 +83,49 @@ exports.writeMessage = (message, username = 'anonymous', roomname = 'lobby') => 
   .catch((err) => console.error(err));
 };
 
-// const chat = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'plantlife',
-//   database: 'chat'
-// });
 
-// exports.readMessages = (callback, user = null) => {
-//
-//   if (user) {
-//     chat.query('SELECT * FROM `messages` WHERE `username` = "'+ user + '"', (err, results) => {
-//       if (err) {
-//         throw err;
-//       }
-//       callback(results);
-//
-//     });
-//   } else {
-//     chat.query('SELECT * FROM `messages`', (err, results) => {
-//       if (err) {
-//         throw err;
-//       }
-//       callback(results);
-//
-//     });
-//   }
-// };
+///////////////////////////////////////////////////
+//// VANILLA SQL IMPLEMENTATION (NO SEQUELIZE) ////
+///////////////////////////////////////////////////
 
-// exports.readMessages((results) => { console.log(results); });
-// exports.writeMessage = (message, username = 'anonymous', roomname = 'lobby') => {
-//   //add column to the messages DB table
-//   chat.query('INSERT INTO `messages` (username, roomname, text) VALUES ("' + username + '", "'+ roomname + '", "' + message +'")', (err, results) => {
-//     if (err) {
-//       throw err;
-//     }
-//   });
-// };
+var mysql = require('mysql');
+
+const chat = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'plantlife',
+  database: 'chat'
+});
+
+exports.readMessages = (callback, user = null) => {
+
+  if (user) {
+    chat.query('SELECT * FROM `messages` WHERE `username` = "'+ user + '"', (err, results) => {
+      if (err) {
+        throw err;
+      }
+      callback(results);
+
+    });
+  } else {
+    chat.query('SELECT * FROM `messages`', (err, results) => {
+      if (err) {
+        throw err;
+      }
+      callback(results);
+
+    });
+  }
+};
+
+exports.readMessages((results) => { console.log(results); });
+exports.writeMessage = (message, username = 'anonymous', roomname = 'lobby') => {
+  //add column to the messages DB table
+  chat.query('INSERT INTO `messages` (username, roomname, text) VALUES ("' + username + '", "'+ roomname + '", "' + message +'")', (err, results) => {
+    if (err) {
+      throw err;
+    }
+  });
+};
+
+*/
